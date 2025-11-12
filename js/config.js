deb1d8df6a6acd97d99e15e5c68b078cce793e1d
SinghHarjyot <harjyotsingh.formal@gmail.com>
2025-11-13 02:07:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,11 @@
+// config.js
 const config = {
-  apiBaseUrl: "https://mock.minijira.api"
+  apiBaseUrl: "https://api.minijira.com",
+  cognito: {
+    region: "ap-south-1",
+    userPoolId: "ap-south-1_jmGuJClkz",
+    clientId: "1s0t8bjbs4bvb7jot41s4iigap"
+  }
 };
+
 export default config;
